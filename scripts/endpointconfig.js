ea9d182d776188349f032b50b6cf8799e5e8e3c6
RMAdobe <rmolano@adobe.com>
2025-09-25 09:28:07 -0700

    Update endpointconfig.js

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,11 +1,11 @@
 import { getConfigValue } from './configs.js';
 
 function getAEMPublish() {
-  return getConfigValue('https://publish-p153123-e1585310.adobeaemcloud.com');
+  return ('https://publish-p153123-e1585310.adobeaemcloud.com');
 }
 
 function getAEMAuthor() {
-  return getConfigValue('https://author-p153123-e1585310.adobeaemcloud.com');
+  return ('https://author-p153123-e1585310.adobeaemcloud.com');
 }
 
 export { getAEMPublish, getAEMAuthor };
